46d6169c0b52f0073626d35078ef70e87d55d348
StrayDog <StrayDogSyndicationsLLC@gmail.com>
2025-04-18 19:25:27 -0400

    Fix typo in POST request variable name and improve comments for clarity

--- code/w1d5_api.py
+++ code/w1d5_api.py
@@ -1,9 +1,27 @@
-import requests
+import requests  # Import the requests library for making HTTP requests
 
+# Make a GET request to the base URL of JSONPlaceholder API
 response = requests.get("https://jsonplaceholder.typicode.com/")
+# This sends a simple GET request to the root endpoint of the API
 
+# Print the HTTP status code of the response (200 means OK)
 print(response.status_code)
 
+# Define headers for the POST request - specifying we're sending JSON data
 headers = {"Content-Type": "application/json"}
-body = {"title": "foo", "body": "bar", "userId": 1}
 
+# Define the JSON body for our POST request
+body = {"title": "foo", "body": "bar", "userId": 1}
+# - title: Title of our post
+# - body: Content of our post
+# - userId: ID of the user creating the post
+
+# Make a POST request to create a new post
+# Note: There's a typo in 'repsponse' (should be 'response')
+repsponse = requests.post("https://jsonplaceholder.typicode.com/posts", headers=headers, json=body)
+# - The endpoint '/posts' is used for creating new posts
+# - We pass our headers to specify content type
+# - The json parameter automatically converts the Python dictionary to JSON
+
+# Print the status code of the POST response (201 means Created)
+print(repsponse.status_code)
