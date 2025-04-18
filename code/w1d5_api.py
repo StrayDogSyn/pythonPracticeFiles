b43fadbde04eacddcdf499e6f123791adb21b908
StrayDog <StrayDogSyndicationsLLC@gmail.com>
2025-04-18 19:27:36 -0400

    Fix typo in POST request variable name and improve print statements for clarity

--- code/w1d5_api.py
+++ code/w1d5_api.py
@@ -5,7 +5,7 @@ response = requests.get("https://jsonplaceholder.typicode.com/")
 # This sends a simple GET request to the root endpoint of the API
 
 # Print the HTTP status code of the response (200 means OK)
-print(response.status_code)
+print(f'This is the 1st: {response.status_code}')
 
 # Define headers for the POST request - specifying we're sending JSON data
 headers = {"Content-Type": "application/json"}
@@ -18,10 +18,10 @@ body = {"title": "foo", "body": "bar", "userId": 1}
 
 # Make a POST request to create a new post
 # Note: There's a typo in 'repsponse' (should be 'response')
-repsponse = requests.post("https://jsonplaceholder.typicode.com/posts", headers=headers, json=body)
+response = requests.post("https://jsonplaceholder.typicode.com/posts", headers=headers, json=body)
 # - The endpoint '/posts' is used for creating new posts
 # - We pass our headers to specify content type
 # - The json parameter automatically converts the Python dictionary to JSON
 
 # Print the status code of the POST response (201 means Created)
-print(repsponse.status_code)
+print(f'This is the 2nd: {response.status_code}')
